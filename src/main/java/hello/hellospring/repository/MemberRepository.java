aaa72e5d1afed205fa34d9c3baeb5daa3f8d44cf
quddkflr <nmtoy342@naver.com>
2024-11-18 21:58:47 +0900

    study 14

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -9,5 +9,8 @@ public interface MemberRepository {
     Member save(Member member);;
     Optional<Member> findById(long id);
     Optional<Member> findByName(String name);
+
+    Optional<Member> findById(Long id);
+
     List<Member> findAll();
 }
