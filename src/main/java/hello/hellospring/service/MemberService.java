b0e0d463ba0f63975a9800d3e09c1f42c5288a0c
quddkflr <nmtoy342@naver.com>
2024-11-23 22:48:24 +0900

    study 19

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -19,9 +19,18 @@ public class MemberService {
 
     public Long join(Member member) {
 
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "mx");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -33,7 +42,14 @@ public class MemberService {
 
 
     public List<Member> findMembers() {
-        return  memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return  memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "mx");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
